5223a34766aaf34cd3a9c9277ae0f554afaca860
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2020-12-23 16:04:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,30 @@ app = Flask(__name__)
 @app.route('/post',methods=['POST','GET'])
 def post():
  data1  =request.get_json()
- return data1["data_i"]
+ value=data1["data_i"]
+ dataset = pd.read_csv('Mineral.csv')
+ x = dataset.iloc[:, :-1]
+ y = dataset.iloc[:, -1]
+ print(x)
+ print(y)
+
+ from sklearn.preprocessing import LabelEncoder
+ encoder = LabelEncoder()
+ y = encoder.fit_transform(y)
+ print(y)
+
+ from sklearn.model_selection import train_test_split
+ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=2)
+
+ from sklearn.tree import DecisionTreeClassifier
+ classifier = DecisionTreeClassifier(criterion='entropy', max_leaf_nodes=1000)
+ classifier.fit(x_train, y_train)
+
+ y_p = classifier.predict([[value]])
+ mineral_name = encoder.inverse_transform(y_p)
+ print(mineral_name)
+ data1["min_name"]= mineral_name
+ return data1
 
 
  
