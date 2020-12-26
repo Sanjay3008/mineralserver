dee2194deabae23fd62d8ff0268b4056bd85cfda
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2020-12-26 21:10:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,17 @@ from flask import Flask, request, jsonify,render_template
 
 app = Flask(__name__)
 
- 
-@app.route('/post',methods=['POST','GET'])
-def post():
- data1  =request.get_json()
- value=data1["data_i"]
+
+def min_pred(value):
  dataset = pd.read_csv('Mineral.csv')
  x = dataset.iloc[:, :-1]
  y = dataset.iloc[:, -1]
- print(x)
- print(y)
+ 
 
  from sklearn.preprocessing import LabelEncoder
  encoder = LabelEncoder()
  y = encoder.fit_transform(y)
- print(y)
+ 
 
  from sklearn.model_selection import train_test_split
  x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=2)
@@ -29,8 +25,13 @@ def post():
 
  y_p = classifier.predict([[value]])
  mineral_name = encoder.inverse_transform(y_p)
- data1["min_name"]= mineral_name
+ return str(mineral_name)
  
+@app.route('/post',methods=['POST','GET'])
+def post():
+ data1  =request.get_json()
+ value=data1["data_i"]
+ data1["min_name"] = min_pred(value)
  return data1
 
 
