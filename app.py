fbeab34d67be76ca015fe78ce8a2baf3a84e9a4b
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2021-03-15 23:36:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,35 @@ from flask import Flask, request, jsonify,render_template
 app = Flask(__name__)
 
 
+def min_pred(value):
+ dataset = pd.read_csv('Mineral.csv')
+ x = dataset.iloc[:, :-1]
+ x = np.int32(np.around(x * 10 ** 4))
+ y = dataset.iloc[:, -1]
+
+ min = np.min(x)
+ max = np.max(x)
+ min_e = min - 4
+ max_e = max + 4
+ if (((int)(value) < min_e) | ((int)(value) > max_e)):
+     return 'No_Element'
+
+ from sklearn.preprocessing import LabelEncoder
+ encoder = LabelEncoder()
+ y = encoder.fit_transform(y)
+
+ from sklearn.model_selection import train_test_split
+ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=2)
+
+ from sklearn.tree import DecisionTreeClassifier
+ classifier = DecisionTreeClassifier(criterion='entropy', max_leaf_nodes=1000)
+ classifier.fit(x, y)
+
+ y_p = classifier.predict([[value]])
+ mineral_name = encoder.inverse_transform(y_p)
+ return str(mineral_name)
+
+
 def min_pred_air(value):
  dataset  = pd.read_csv('Mineral_.csv')
  x = dataset.iloc[:,2].values
@@ -34,35 +63,7 @@ def min_pred_air(value):
  mineral_name = label_encoder.inverse_transform(y_p)
  return str(mineral_name)
 
-def min_pred(value):
- dataset = pd.read_csv('Mineral.csv')
- x = dataset.iloc[:, :-1]
- x = (x * 10000)
 
- value = (value * 10000)
- y = dataset.iloc[:, -1]
-
-#    min = np.min(x)
-#    max = np.max(x)
-#    min_e = min - 4
-#    max_e = max + 4
-#    if (((int)(value) < min_e) | ((int)(value) > max_e)):
-#        return 'No_Element'
-
- from sklearn.preprocessing import LabelEncoder
- encoder = LabelEncoder()
- y = encoder.fit_transform(y)
-
- from sklearn.model_selection import train_test_split
- x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=2)
-
- from sklearn.tree import DecisionTreeClassifier
- classifier = DecisionTreeClassifier(criterion='entropy', max_leaf_nodes=1000)
- classifier.fit(x, y)
-
- y_p = classifier.predict([[value]])
- mineral_name = encoder.inverse_transform(y_p)
- return str(mineral_name)
  
 @app.route('/post',methods=['POST','GET'])
 def post():
