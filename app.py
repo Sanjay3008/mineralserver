499a0b8db4717b7ae1d8a7990dd1ea0d19ef2b8b
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2021-03-15 18:53:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def min_pred_air(value):
 def min_pred(value):
  dataset = pd.read_csv('Mineral.csv')
  x = dataset.iloc[:, :-1]
- 
+ x= np.int32(np.around(x * 10**4))
  y = dataset.iloc[:, -1]
  min=np.min(x)
  max= np.max(x)
